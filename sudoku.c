9bd452dfc88b442abe82b19c1baf01f6e2eb1e4c
Bhuvansh <bhuvanshgoyal2004@gmail.com>
2023-05-11 04:02:06 +0530

    Added solution counter function

--- sudoku.c
+++ sudoku.c
@@ -46,6 +46,10 @@ int next_empty_cell(int board[][9]) {
     return -1;
 }
 
+int generate_board(int board[][9]) {
+
+}
+
 int solve_board(int board[][9], int current_pos[]) {
     for (size_t i = 1; i <= 9; i++) {
         if (is_valid(board, current_pos, i)) {
@@ -70,6 +74,39 @@ int solve_board(int board[][9], int current_pos[]) {
     return -1;
 }
 
-int main() {
+int count_solutions(int board[][9], int current_pos[], int* count, int count_all) {
+    for (size_t i = 1; i <= 9; i++) {
+        if (is_valid(board, current_pos, i)) {
+            board[current_pos[0]][current_pos[1]] = i;
 
+            int temp = next_empty_cell(board);
+            int next_empty_pos[2] = {temp / 10, temp % 10};
+            if ((temp == -1)) {
+                *(count) += 1;
+                board[current_pos[0]][current_pos[1]] = 0;
+                return *(count);
+            }
+
+            if (count_solutions(board, next_empty_pos, count, count_all) > 1) {
+                if (!count_all) {
+                    board[current_pos[0]][current_pos[1]] = 0;
+                    return *(count);
+                } 
+            }
+        }
+    }
+    
+    board[current_pos[0]][current_pos[1]] = 0;
+    return *(count);
+}
+
+int main() {
+    int board[9][9] = {2, 9, 5, 7, 4, 3, 8, 6, 1, 4, 3, 1, 8, 6, 5, 9, 0, 0, 8, 7, 6, 1, 9, 2, 5, 4, 3, 3, 8, 7, 4, 5, 9, 2, 1, 6, 6, 1, 2, 3, 8, 7, 4, 9, 5, 5, 4, 9, 2, 1, 6, 7, 3, 8, 7, 6, 3, 5, 3, 4, 1, 8, 9, 9, 2, 8, 6, 7, 1, 3, 5, 4, 1, 5, 4, 9, 3, 8, 6, 0, 0};
+    int zeros[9][9] = {0};
+    int temp = next_empty_cell(zeros);
+    int next_empty_pos[2] = {temp / 10, temp % 10};
+    int count = 0;
+    count_solutions(zeros, next_empty_pos, &count, 0);
+    printf("%d\n", count);
+    print_board(zeros);
 }
