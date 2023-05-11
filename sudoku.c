b54bf16bf3543946c0942c22ea3d4b9d7347fb3e
Bhuvansh <bhuvanshgoyal2004@gmail.com>
2023-05-11 18:42:30 +0530

    Added random filling method

--- sudoku.c
+++ sudoku.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
-
-void init_board(int board[][9]);
+#include<stdlib.h>
+#include<time.h>
 
 void print_board(int board[][9]) {
     for (size_t row = 0; row < 9; row++) {
@@ -46,8 +46,73 @@ int next_empty_cell(int board[][9]) {
     return -1;
 }
 
-int generate_board(int board[][9]) {
+void shuffle(int array[], int size) {
+    for (size_t i = 0; i < size; i++) {
+        int j;
+        do
+        {
+            j = rand() % size;
+        } while (i == j);
+        
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    
+}
 
+int fill_board(int board[][9], int current_pos[], int counter, const int values[], int stop) {
+    for (size_t i = 1; i <= 9; i++) {
+        if (is_valid(board, current_pos, values[i])) {
+            board[current_pos[0]][current_pos[1]] = values[i];
+
+            int temp = next_empty_cell(board);
+            int next_empty_pos[2] = {temp / 10, temp % 10};
+            if ((temp == -1)) {
+                counter += 1;
+                if (!(counter > (stop-1))) {
+                    board[current_pos[0]][current_pos[1]] = 0;
+                }
+                return counter;
+            }
+
+            int new_vals[9];
+            for (size_t i = 0; i < 9; i++) { 
+                new_vals[i] = values[i];
+            }
+            shuffle(new_vals, 9);
+
+            counter = fill_board(board, next_empty_pos, counter, new_vals, stop);
+
+            if (counter > (stop-1)) {
+                return counter;
+            }
+        }
+    }
+    
+    board[current_pos[0]][current_pos[1]] = 0;
+    return counter;
+}
+
+void generate_board(int board[][9]) {
+    int ans = 0;
+    int stop = 1 + rand() % 200;
+
+    int temp = next_empty_cell(board);
+    int next_empty_pos[2] = {temp / 10, temp % 10};
+
+    int initial[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    shuffle(initial, 9);
+
+    do
+    {
+        for (size_t i = 0; i < 9; i++) {
+            for (size_t j = 0; j < 9; j++) {
+                board[i][j] = 0;
+            }
+        }
+        ans = fill_board(board, next_empty_pos, 0, initial, stop);
+    } while (ans < stop);
 }
 
 int solve_board(int board[][9], int current_pos[]) {
@@ -109,11 +174,10 @@ int count_solutions(int board[][9], int current_pos[], int counter, int count_ti
 }
 
 int main() {
+    srand(time(NULL));
+
     int board[9][9] = {2, 9, 5, 7, 4, 3, 8, 6, 1, 4, 3, 1, 8, 6, 5, 9, 0, 0, 8, 7, 6, 1, 9, 2, 5, 4, 3, 3, 8, 7, 4, 5, 9, 2, 1, 6, 6, 1, 2, 3, 8, 7, 4, 9, 5, 5, 4, 9, 2, 1, 6, 7, 3, 8, 7, 6, 3, 5, 3, 4, 1, 8, 9, 9, 2, 8, 6, 7, 1, 3, 5, 4, 1, 5, 4, 9, 3, 8, 6, 0, 0};
     int zeros[9][9] = {0};
-    int temp = next_empty_cell(board);
-    int next_empty_pos[2] = {temp / 10, temp % 10};
-    int count = 0;
-    count_solutions(board, next_empty_pos, 0, 5, 1);
+    generate_board(zeros);
     print_board(zeros);
 }
